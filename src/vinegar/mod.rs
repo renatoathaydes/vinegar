b922f1255923ba20ec78dbd5a7e29f718f079fe3
renato <renato@twobo.com>
2017-10-10 14:04:24 +0200

    Fixed indentation.

--- src/vinegar/mod.rs
+++ src/vinegar/mod.rs
@@ -39,69 +39,69 @@ pub fn build_error(bs: &str, be: &str, op: &str, astr: &str, ae: &str) -> String
 #[macro_export]
 macro_rules! expect {
 
-  (|| $b:block $op:tt || $a:block) => {{
-      if $b $op $a {
-          Result::Ok(())
-      } else {
-          Result::Err(::vinegar::build_error(
-              &format!("{:?}", $b), stringify!($b), stringify!($op),
-              &format!("{:?}", $a), stringify!($a)))
-      }
-  }};
+    (|| $b:block $op:tt || $a:block) => {{
+        if $b $op $a {
+            Result::Ok(())
+        } else {
+            Result::Err(::vinegar::build_error(
+                &format!("{:?}", $b), stringify!($b), stringify!($op),
+                &format!("{:?}", $a), stringify!($a)))
+        }
+    }};
 
-  (|| $b:block $($a:tt)+) => {{
-      if $b $($a)* {
-          Result::Ok(())
-      } else {
-          let bs = format!("{:?}", $b);
-          let be = stringify!($b);
-          // quotes are rendered with an escape character, so we need to add to the length
-          let belen = be.len() + be.matches("\"").count();
-          let spaces = "                    ";
-          let underlines = "-".repeat(belen);
-          let arrow_line_spaces = " ".repeat(belen / 2);
-          let arrow_line = format!("{}{}|", spaces, arrow_line_spaces);
-          let val_line = format!("{}{}{}", spaces, arrow_line_spaces, bs);
-          Result::Err(format!("* Condition failed: {} {}\n{}{}\n{}\n{}\n",
-            be, stringify!($($a)*), spaces, underlines, arrow_line, val_line))
-      }
-  }};
+    (|| $b:block $($a:tt)+) => {{
+        if $b $($a)* {
+            Result::Ok(())
+        } else {
+            let bs = format!("{:?}", $b);
+            let be = stringify!($b);
+            // quotes are rendered with an escape character, so we need to add to the length
+            let belen = be.len() + be.matches("\"").count();
+            let spaces = "                    ";
+            let underlines = "-".repeat(belen);
+            let arrow_line_spaces = " ".repeat(belen / 2);
+            let arrow_line = format!("{}{}|", spaces, arrow_line_spaces);
+            let val_line = format!("{}{}{}", spaces, arrow_line_spaces, bs);
+            Result::Err(format!("* Condition failed: {} {}\n{}{}\n{}\n{}\n",
+                be, stringify!($($a)*), spaces, underlines, arrow_line, val_line))
+        }
+    }};
 
-  ($($a:tt)*) => {{
-      if $($a)* {
-          Result::Ok(())
-      } else {
-          Result::Err(format!("Condition failed: {}", stringify!($($a)*)))
-      }
-  }};
+    ($($a:tt)*) => {{
+        if $($a)* {
+            Result::Ok(())
+        } else {
+            Result::Err(format!("Condition failed: {}", stringify!($($a)*)))
+        }
+    }};
 
 }
 
 #[macro_export]
 macro_rules! expect_eq {
-  ($a:expr, $b: expr) => {{
-      if $a == $b {
-          Result::Ok(())
-      } else {
-          Result::Err(format!("Equality failed: {:?} != {:?}", $a, $b))
-      }
-  }}
+    ($a:expr, $b: expr) => {{
+        if $a == $b {
+            Result::Ok(())
+        } else {
+            Result::Err(format!("Equality failed: {:?} != {:?}", $a, $b))
+        }
+    }}
 }
 
 #[macro_export]
 macro_rules! expect_len {
-  ($a:expr, $b: expr) => {{
-      use std::cmp::min;
-      if $a.len() == $b {
-          Result::Ok(())
-      } else {
-          let a_str = format!("{:?}", $a);
-          let max_i = min(a_str.chars().count(), 50);
-          let mut short_a_str = a_str[..max_i].to_string();
-          if short_a_str.chars().count() < a_str.chars().count() {
-            short_a_str = format!("{}...", short_a_str);
-          }
-          Result::Err(format!("Length of {} is {}, not {:?} -- {}",
+    ($a:expr, $b: expr) => {{
+        use std::cmp::min;
+        if $a.len() == $b {
+            Result::Ok(())
+        } else {
+            let a_str = format!("{:?}", $a);
+            let max_i = min(a_str.chars().count(), 50);
+            let mut short_a_str = a_str[..max_i].to_string();
+            if short_a_str.chars().count() < a_str.chars().count() {
+                short_a_str = format!("{}...", short_a_str);
+            }
+            Result::Err(format!("Length of {} is {}, not {:?} -- {}",
                 stringify!($a), $a.len(), $b, short_a_str))
       }
   }}
